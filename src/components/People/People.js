8c2f43d1f74b2c4e836edfb2ccfbbde2bf9de65c
Moore-ConnorS <littlemoorepower@Connors-MacBook-Pro.local>
2018-12-04 12:49:34 -0700

    finished lecture

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
-import { getPeople } from '../../usersService.js';
+import React, { Component } from "react";
+import { getPeople } from "../../usersService.js";
+import { Link } from "react-router-dom";
 
 export default class People extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       people: []
-    }
+    };
   }
 
   componentDidMount() {
     // axios call would go here to get data
-    let people = getPeople() // getting people data from service file
+    let people = getPeople(); // getting people data from service file
     this.setState({
       people: people // updating state with people data
-    })
+    });
   }
 
   render() {
     const people = this.state.people.map((e, i) => {
       return (
-        <h3>{e.name}</h3>
-      )
-    })
+        <h3 key={e.id}>
+          <Link to={`/people/${e.id}`}>{e.name}</Link>
+        </h3>
+      );
+    });
     return (
       <div>
-        <h1>This is the people component</h1>
+        <h1>This is the People component</h1>
         {people}
       </div>
-    )
+    );
   }
 }
